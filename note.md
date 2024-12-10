a3a4c5add8f7a3ef4dfa4ede4317ebe92dbc10c9
parukazz <parukazziyi@gmail.com>
2024-12-10 14:57:43 +0700

    initial commit

---
+++ note.md
@@ -0,0 +1,6 @@
+Node.js is a Javascript runtime built on Chrome's V8 JavaScript Engine
+
+Node.js itu runtime dimana kita bisa menjalankan javascript di dalamnya (so nodejs is not language)
+
+runtime environment for executing javascript code
+
---
+++ try.js
@@ -0,0 +1,34 @@
+// Synchronus
+// const getUserSync = (id) => {
+//   const name = id === 1 ? "Paruk" : "Azhari";
+//   return { id, name };
+// };
+
+// const userSatu = getUserSync(1);
+// console.log(userSatu);
+
+// const userDua = getUserSync(2);
+// console.log(userDua);
+
+// const halo = "Hello World!";
+// console.log(halo);
+
+// Asynchronus
+const getUser = (id, callback) => {
+  const time = id === 1 ? 3000 : 2000;
+  setTimeout(() => {
+    const name = id === 1 ? "Paruk" : "Azhari";
+    callback({ id, name });
+  }, time);
+};
+
+const userSatu = getUser(1, (hasil) => {
+  console.log(hasil);
+});
+
+const userDua = getUser(2, (hasil) => {
+  console.log(hasil);
+});
+
+const halo = "Hello World";
+console.log("selesai");
